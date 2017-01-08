075d251d412ea514e5a8f097820c599ccb18778a
Arti <hvoostik@gmail.com>
2017-01-08 19:19:50 +0200

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,62 @@
 $(document).ready(function() {
+	Array.prototype.slice.call(document.querySelectorAll('.js_slider')).forEach(function (element, index) {
+		element.addEventListener('before.lory.slide', handleSliderArrows);
+
+		lory(element, {
+			rewindOnResize: false,
+		});
+	});
+
+	function handleSliderArrows(e) {
+		var slidesCount = e.target.querySelectorAll('.js_slide').length;
+		var nextSlide = e.detail.nextSlide;
+		var currentSlide = e.detail.index;
+
+		if (nextSlide > currentSlide) {
+			switchArrow('prev', true, e.target);
+
+			if (nextSlide >= slidesCount - 1) {
+				switchArrow('next', false, e.target);
+			}
+		} else if (nextSlide < currentSlide) {
+			switchArrow('next', true, e.target);
+
+			if (nextSlide <= 0) {
+				switchArrow('prev', false, e.target);
+			}
+		}
+	}
+
+	function switchArrow(direction, state, slider) {
+		var disabledClass = 'slider__arrow--disabled';
+		var arrow = slider.querySelector('.js_' + direction);
+
+		if (state) {
+			arrow.classList.remove(disabledClass);
+		} else {
+			arrow.classList.add(disabledClass);
+		}
+	}
+
+	function fireRefreshEventOnWindow() {
+		var event = document.createEvent("HTMLEvents");
+
+		event.initEvent('resize', true, false);
+		window.dispatchEvent(event);
+	}
+
 	$('.faq__question').click(function() {
 		$(this).parent().toggleClass('is-expanded');
 	});
 
-	$('.photos-list').bxSlider({
-		pager: false,
-		infiniteLoop: false,
-		hideControlOnEnd: true
+	$('.tab-card').on('click', function(e) {
+		e.preventDefault();
+
+		$(this)
+			.addClass('tab-card--current').siblings().removeClass('tab-card--current')
+			.closest('.products').find('.products__tab').removeClass('products__tab--current').eq($(this).index()).addClass('products__tab--current');
+
+		fireRefreshEventOnWindow();
 	});
 
 	$('.masthead__action').click(function(e) {
@@ -19,64 +69,225 @@ $(document).ready(function() {
 
 		e.preventDefault();
 	});
-
-	function showPopup() {
-		// using two separate style sets, otherwise they applied in the wrong order
-		$('html')
-			.css({
-				overflowY: 'hidden',
-			})
-			.css({
-				paddingRight: getScrollbarSize() + 'px',
-			});
-
-		$('.popup__container').scrollTop = 0;
-		$('html').addClass('is-popup-visible');
-		$('.popup').addClass('popup--visible');
-	}
-
-	function hidePopup() {
-		$('html').removeClass('is-popup-visible');
-		$('.popup').removeClass('popup--visible');
-
-		setTimeout(function () {
-			$('html').css({
-				overflowY: 'scroll',
-				paddingRight: 0,
-			});
-		}, 300);
-	}
-
-	function getScrollbarSize() {
-		var scrollDiv = document.createElement('div');
-		var scrollbarSize;
-
-		scrollDiv.style.cssText = 'width: 99px; height: 99px; overflow: scroll; position: absolute; top: -9999px;';
-		document.body.appendChild(scrollDiv);
-		scrollbarSize = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-		document.body.removeChild(scrollDiv);
-
-		return scrollbarSize;
-	}
-
-	$('.js-open-popup').click(function (e) {
-		e.preventDefault();
-
-		showPopup();
-	});
-
-	$('.js-close-popup').click(function (e) {
-		if (e.target.classList.contains('js-close-popup')) {
-			e.preventDefault();
-
-			hidePopup();
-		}
-	});
-
-	$(document).keydown(function (e) {
-		// ESCAPE key pressed
-		if (e.keyCode === 27) {
-			hidePopup();
-		}
-	});
 });
+
+// Shopify buttons
+(function() {
+	var options = {
+		"product": {
+			"variantId": "all",
+			"width": "240px",
+			"contents": {
+				"img": false,
+				"title": false,
+				"variantTitle": false,
+				"price": false,
+				"description": false,
+				"buttonWithQuantity": true,
+				"button": false,
+				"quantity": false
+			},
+			"text": {
+				"button": "Add to cart"
+			},
+			"styles": {
+				"product": {
+					"text-align": "left",
+					"@media (min-width: 601px)": {
+						"max-width": "calc(25% - 20px)",
+						"margin-left": "20px",
+						"margin-bottom": "50px"
+					}
+				},
+				"button": {
+					"background-color": "#ff4324",
+					"font-size": "14px",
+					"padding-top": "7px",
+					"padding-bottom": "7px",
+					"padding-left": "20px",
+					"padding-right": "20px",
+					":hover": {
+						"background-color": "#e63c20"
+					},
+					":focus": {
+						"background-color": "#e63c20"
+					},
+					"font-weight": "bold"
+				},
+				"quantityInput": {
+					"font-size": "14px",
+					"padding-top": "7px",
+					"padding-bottom": "7px"
+				},
+				"buttonWithQuantity": {
+					"margin-top": "10px !important"
+				}
+			}
+		},
+		// "option": {},
+		"cart": {
+			"contents": {
+				"button": true
+			},
+			"styles": {
+				"button": {
+					"background-color": "#ff4324",
+					"font-size": "14px",
+					"padding-top": "15px",
+					"padding-bottom": "15px",
+					":hover": {
+						"background-color": "#e63c20"
+					},
+					":focus": {
+						"background-color": "#e63c20"
+					},
+					"font-weight": "bold"
+				},
+				"footer": {
+					"background-color": "#ffffff"
+				}
+			}
+		},
+		"modalProduct": {
+			"contents": {
+				"variantTitle": false,
+				"buttonWithQuantity": true,
+				"button": false,
+				"quantity": false
+			},
+			"styles": {
+				"product": {
+					"@media (min-width: 601px)": {
+						"max-width": "100%",
+						"margin-left": "0px",
+						"margin-bottom": "0px"
+					}
+				},
+				"button": {
+					"background-color": "#ff4324",
+					"font-size": "14px",
+					"padding-top": "15px",
+					"padding-bottom": "15px",
+					"padding-left": "30px",
+					"padding-right": "30px",
+					":hover": {
+						"background-color": "#e63c20"
+					},
+					":focus": {
+						"background-color": "#e63c20"
+					},
+					"font-weight": "bold"
+				},
+				"quantityInput": {
+					"font-size": "14px",
+					"padding-top": "15px",
+					"padding-bottom": "15px"
+				}
+			}
+		},
+		"toggle": {
+			"styles": {
+				"toggle": {
+					"background-color": "#ff4324",
+					":hover": {
+						"background-color": "#e63c20"
+					},
+					":focus": {
+						"background-color": "#e63c20"
+					},
+					"font-weight": "bold"
+				},
+				"count": {
+					"font-size": "14px"
+				}
+			}
+		},
+		"productSet": {
+			"styles": {
+				"products": {
+					"@media (min-width: 601px)": {
+						"margin-left": "-20px"
+					}
+				}
+			}
+		}
+	};
+
+	// Sketch
+	(function () {
+		var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+		if (window.ShopifyBuy) {
+			if (window.ShopifyBuy.UI) {
+				ShopifyBuyInit();
+			} else {
+				loadScript();
+			}
+		} else {
+			loadScript();
+		}
+
+		function loadScript() {
+			var script = document.createElement('script');
+			script.async = true;
+			script.src = scriptURL;
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+			script.onload = ShopifyBuyInit;
+		}
+
+		function ShopifyBuyInit() {
+			var client = ShopifyBuy.buildClient({
+				domain: 'sketchkeys.myshopify.com',
+				apiKey: '655a916cfa5f1eab28d2b0b6ef44d7ac',
+				appId: '6',
+			});
+
+			ShopifyBuy.UI.onReady(client).then(function (ui) {
+				ui.createComponent('product', {
+					id: [5389615873],
+					node: document.getElementById('product-component-466273057e4'),
+					moneyFormat: '%24%7B%7Bamount%7D%7D',
+					options: options
+				});
+			});
+		}
+	})();
+
+	// XCode
+	(function () {
+		var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+		if (window.ShopifyBuy) {
+			if (window.ShopifyBuy.UI) {
+				ShopifyBuyInit();
+			} else {
+				loadScript();
+			}
+		} else {
+			loadScript();
+		}
+
+		function loadScript() {
+			var script = document.createElement('script');
+			script.async = true;
+			script.src = scriptURL;
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+			script.onload = ShopifyBuyInit;
+		}
+
+		function ShopifyBuyInit() {
+			var client = ShopifyBuy.buildClient({
+				domain: 'sketchkeys.myshopify.com',
+				apiKey: '655a916cfa5f1eab28d2b0b6ef44d7ac',
+				appId: '6',
+			});
+
+			ShopifyBuy.UI.onReady(client).then(function (ui) {
+				ui.createComponent('product', {
+					id: [8753894797],
+					node: document.getElementById('product-component-cf6e5c2ebd5'),
+					moneyFormat: '%24%7B%7Bamount%7D%7D',
+					options: options
+				});
+			});
+		}
+	})();
+})();
